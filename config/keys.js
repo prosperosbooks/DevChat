6622e586565be8f94693176d73c73392906557a7
prosperosbooks <allen.emin@gmail.com>
2018-05-31 23:31:02 -0400

    user API routes and JWT authentication in express

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://emin:emin@ds235860.mlab.com:35860/devchat5"
+  mongoURI: "mongodb://emin:emin@ds235860.mlab.com:35860/devchat5",
+  secretOrKey: "secret"
 };
